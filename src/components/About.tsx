95c159781058fb17911c643b98c93e32a35658b6
thym101 <tmaiaa@siue.edu>
2025-05-06 16:33:27 -0500

    Update About me

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,9 +9,23 @@ const About = () => {
             <div className="about-text">
             <h2 className="title">WHO I AM?</h2>
             <p>
-                My name is Thy Mai, and I am currently a Junior at Southern Illinois University Edwardsville, majoring in Computer Science. I am a quick and enthusiastic learner with a strong passion for self-improvement and continuous growth. I enjoy exploring new technologies and thrive on solving challenging problems. My interests span across various areas of web application development, and I am committed to contributing to a more open and accessible web.
-                I am actively expanding my knowledge in programming languages such as Python, C++, HTML/CSS, SQL, and JavaScript. I am open to any job opportunities that align with my skills and passion for technology.
-                In the future, I aspire to become a full-stack developer and eventually a tech lead, where I can drive innovative projects, mentor others, and build impactful digital solutions that make a difference in people's lives.
+                My name is <strong>Thy Mai</strong>, and I am currently a Junior at <a href="https://www.siue.edu/" target='blank'><strong>Southern Illinois University Edwardsville</strong></a>
+            </p>
+
+            <p>
+                <strong>Major</strong>: Computer Science. 
+            </p>
+
+            <p><strong>GPA</strong>: 4.0/4.0</p>
+            <p><strong>Expected Graduation Year</strong>: December 2026</p>
+            <p>    
+                <strong>Fun fact</strong>: I enjoy exploring new technologies and thrive on solving challenging problems. My interests span across various areas of web application development, and I am committed to contributing to a more open and accessible web.
+            </p>
+            <p>
+                <strong>Skills</strong>: I am actively expanding my knowledge in programming languages such as Python, C++, HTML/CSS, SQL, and JavaScript. 
+            </p>
+            <p>
+                <strong>Future plan</strong>: I aspire to become a full-stack developer and eventually a tech lead, where I can drive innovative projects, mentor others, and build impactful digital solutions that make a difference in people's lives.
             </p>
             </div>
 
